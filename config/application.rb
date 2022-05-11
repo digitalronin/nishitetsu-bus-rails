7e67b4dfb06a07986a7969bc8520cc83ff99144f
digitalronin <david@digitalronin.com>
2022-05-11 09:35:40 +0900

    Tweak rails generator config
    - generate rspec tests
    - don't generate a stylesheet per controller
    - don't generate a helper per controller

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,13 @@ module NishitetsuBusRails
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.generators do |g|
+      g.test_framework :rspec, fixture: false
+      g.stylesheets false
+      g.helper false
+    end
+
     # Don't generate system test files.
-    config.generators.system_tests = nil
+    # config.generators.system_tests = nil
   end
 end
