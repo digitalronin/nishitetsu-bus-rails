98af5336052a4f02ae648e4e54ff49b34d99a3d7
digitalronin <david@digitalronin.com>
2022-05-21 12:02:27 +0900

    Annotate models and run standardrb --fix

--- app/models/bus_route.rb
+++ app/models/bus_route.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: bus_routes
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class BusRoute < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
--- app/models/bus_stop.rb
+++ app/models/bus_stop.rb
@@ -1,8 +1,33 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: bus_stops
+#
+#  id               :bigint           not null, primary key
+#  upd_date         :string
+#  jigyosha_name    :string
+#  tei_kana         :string
+#  ud_type          :string
+#  city_name        :string
+#  tei_cd           :string
+#  jigyosha_cd      :string
+#  tei_name         :string
+#  tei_name_foreign :string
+#  tei_type         :string
+#  city_cd          :string
+#  area_type        :string
+#  latitude         :float
+#  longitude        :float
+#  noriba_cd        :string
+#  community_type   :string
+#  navi_type        :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
 class BusStop < ApplicationRecord
   scope :within_box, lambda { |min_lat:, max_lat:, min_lon:, max_lon:|
-    where('(latitude between ? AND ?) AND (longitude between ? AND ?)', min_lat, max_lat, min_lon, max_lon)
+    where("(latitude between ? AND ?) AND (longitude between ? AND ?)", min_lat, max_lat, min_lon, max_lon)
       .limit(50)
   }
 
@@ -10,7 +35,7 @@ class BusStop < ApplicationRecord
   has_many :bus_routes, through: :route_stops
 
   def api_id
-    [jigyosha_cd, tei_cd].join(',')
+    [jigyosha_cd, tei_cd].join(",")
   end
 
   def connected_stops
@@ -29,6 +54,6 @@ class BusStop < ApplicationRecord
   end
 
   def display_name
-    I18n.locale == :ja ? tei_name : tei_kana.romaji.capitalize.gsub('(', ' (')
+    I18n.locale == :ja ? tei_name : tei_kana.romaji.capitalize.gsub("(", " (")
   end
 end
